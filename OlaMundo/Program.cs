8b5b9f48ba7cc1834c6c1e9e388c2cfbfbf964e4
WillianMGBrdotnet <willianmgbr@hotmail.com>
2022-12-12 14:40:34 -0300

    First project

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //display information in the terminal
+            Console.WriteLine("Hello, World!");
+
+            //press a keyboard key to turn off the terminal
+            Console.ReadKey();
+        }
+    }
+}
