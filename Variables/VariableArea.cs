c1e509e7c895dc67615f2492a38f78638b1eed02
WillianMGBrdotnet <willianmgbr@hotmail.com>
2022-12-12 18:51:25 -0300

    calculation e concatenation of variables

---
+++ Variables/VariableArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables
+{
+    class VariableArea
+    {
+        static void Main()
+        {
+            int x = 4;
+            double y = 3.3;
+
+            //calculation of square area
+            double area = x * y;
+
+            //concatenation of variables
+            //display area in the terminal
+            Console.WriteLine("A area do quadrado é " + area);
+        }
+    }
+}
